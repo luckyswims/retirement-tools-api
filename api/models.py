00c23f748391bb0cbe48199c3a1c00b8baa2cb3a
John Lucker <john.lucker@comcast.net>
2020-08-21 11:05:39 -0400

    Created first model and added package
    Created the annuity model
    Installed djangorestframework package

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Annuity(models.Model):
+    """Defines fields and methods of the Annuity resource model"""
+    amount = models.DecimalField(max_digits=8,decimal_places=2)
+    duration = models.IntegerField()
+    
+    def __str__(self):
+        """Returns a string depiction of the model"""
+        return f"An annuity (ID #{self.pk}) of ${self.amount} per month for {self.duration} months."
+
+    def as_dict(self):
+        """Returns dictionary version of the model"""
+        return {
+            'id': self.id,
+            'amount': self.amount,
+            'duration': self.duration
+        }
