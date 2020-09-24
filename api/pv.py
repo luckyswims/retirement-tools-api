857d87b5bb5106a491979d43c78c94f20f5751d0
John Lucker <john.lucker@comcast.net>
2020-09-24 12:53:43 -0400

    Started work to add deferral
    Added test cases for situations where an annuity is deferred
    Built function for applying the deferral when a single rate is used

--- api/pv.py
+++ api/pv.py
@@ -1,7 +1,14 @@
 def pvSingleRate(amount, duration, rate):
-    v = 1 / (1 + rate)
-    d = rate * v
+    i = ((1 + rate) ** (1/12)) - 1
+    v = 1 / (1 + i)
+    d = i * v
     pv = amount * ((1 - v ** (max(duration, 0))) / d)
+    return pv
+
+def deferralSingleRate(value, rate, deferral):
+    i = ((1 + rate) ** (1/12)) - 1
+    v = 1 / (1 + i)
+    pv = value * v ** deferral
     return pv
 
 def pvPPARates(amount, duration, rates):
@@ -10,9 +17,9 @@ def pvPPARates(amount, duration, rates):
     rate3 = ((1 + rates[2]) ** (1/12)) - 1
     v1 = 1 / (1 + rate1)
     v2 = 1 / (1 + rate2)
-    pv1 = pvSingleRate(amount, min(duration, 60), rate1)
-    pv2 = pvSingleRate(amount, min(duration - 60, 180), rate2)
-    pv3 = pvSingleRate(amount, (duration - 240), rate3)
+    pv1 = pvSingleRate(amount, min(duration, 60), rates[0])
+    pv2 = pvSingleRate(amount, min(duration - 60, 180), rates[1])
+    pv3 = pvSingleRate(amount, (duration - 240), rates[2])
     pv = pv1 + pv2 * (v1 ** 60) + pv3 * (v2 ** 180) * (v1 ** 60)
     return pv
 
